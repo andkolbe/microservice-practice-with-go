5b70c089c9085607e17f3d0eb389259435c717f8
Andrew <kolbe1129@gmail.com>
2021-10-26 16:03:22 -0500

    added route handler to get one article

--- handlers.article.go
+++ handlers.article.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -21,4 +22,31 @@ func showIndexPage(c *gin.Context) {
 			"payload": articles,
 		},
 	)
+}
+
+func getArticle(c *gin.Context) {
+	// check if the article ID is valid
+	if articleID, err := strconv.Atoi(c.Param("article_id")); err == nil {
+		// check if the article exists
+		if article, err := getArticleByID(articleID); err == nil {
+			// call the HTML method of the Context to render a template
+			c.HTML(
+				// set the HTTP status code to 200 (OK)
+				http.StatusOK,
+				// use the index.html template
+				"article.html",
+				// pass the data that the page uses
+				gin.H{
+					"title": article.Title,
+					"payload": article,
+				},
+			)
+		} else {
+			// if the article is not found, abort with an error
+			c.AbortWithError(http.StatusNotFound, err)
+		}
+	} else {
+		// if an invalid article ID is specified in the URL, abort with an error
+		c.AbortWithStatus(http.StatusNotFound)
+	}
 }
--- routes.go
+++ routes.go
@@ -3,4 +3,8 @@ package main
 func initializeRoutes() {
 	// handle the index route
 	router.GET("/", showIndexPage)
+
+	// handle GET requests at /article/view/some_article_id
+	// stores the value of the last part of the route in the route paramenter named article_id which we can access in the route handler
+	router.GET("/article/view/:article_id", getArticle)
 }
